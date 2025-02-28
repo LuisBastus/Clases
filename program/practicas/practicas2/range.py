c295ed552d00f61f81a096311540878a07b1c985
LuisBastus <luiscarlosbastus@gmail.com>
2025-02-28 02:57:30 +0100

    range

--- program/practicas/practicas2/range.py
+++ program/practicas/practicas2/range.py
@@ -1,1 +1,18 @@
-#funcion range
+#!/usr/bin/env python3
+print("##########")
+print("Exercici 1")
+print("##########")
+
+
+# 1.a)
+l = list(range(0,1))
+print(l)
+
+
+# 1.b)
+l = list(range(0,3))
+print(l)
+
+#1.c
+l = list(range(0))
+print(l)
