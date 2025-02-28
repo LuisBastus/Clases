f865c674939c68752617382e10759f1c1735440b
LuisBastus <tuemail@example.com>
2025-02-28 12:53:00 +0100

    range

--- program/practicas/practicas2/range.py
+++ program/practicas/practicas2/range.py
@@ -16,3 +16,23 @@ print(l)
 #1.c
 l = list(range(0))
 print(l)
+
+#1.d
+l = list(range(100 , 104))
+print(l)
+
+#1.e
+l = list(range(100, 6101, 1000))
+print(l)
+
+#1.f
+l = list(range(10, 8011, 2000))
+print(l)
+
+#1.g
+l = list(range(5, -196, -50))
+print(l)
+
+#1.c
+l = list(range(10, -391, -100))
+print(l)
