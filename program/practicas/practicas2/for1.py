dfa9f5931365a3436a1dea6ce6a0866e66f5ff13
LuisBastus <tuemail@example.com>
2025-03-31 01:26:38 +0200

    mssssi

--- program/practicas/practicas2/for1.py
+++ program/practicas/practicas2/for1.py
@@ -1,18 +1,5 @@
 lista = []
 continuar = True 
+num = input("Introduce un numero: ")
 
-while continuar: 
-    print("\nEscge una opcion: ")
-    print("1. Introduce un numero")
-    print("2. Ver lista")
-    
-
-    eleccion = input("Escoge una opcion: ")
-    
-    if eleccion == "1":
-        lista.append(input("introduce un numero: "))
-        
-    elif eleccion == "2":
-        print(lista)
-    
         
