e7a2505f107c433ea80897d01c2810ae26e9a56b
LuisBastus <tuemail@example.com>
2025-04-07 16:33:39 +0200

    committed

---
+++ def.py
@@ -0,0 +1,16 @@
+nombre = input("Escribe tu nombre: ")
+
+def saludar(nombre):
+    print("hola", nombre)
+
+saludar(nombre)
+
+num1 = input("Escribe un numero: ")
+num2= input("Escribe un numero: ")
+num3 = input("Escribe un numero: ")
+
+def media (num1, num2, num3):
+    resulatdo = num1 + num2 + num3
+    resulatdo = resulatdo/3
+    return resulatdo
+
