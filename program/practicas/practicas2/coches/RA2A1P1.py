d8551fa00de4025c49c79fa89ff76283008c93ad
LuisBastus <tuemail@example.com>
2025-05-12 16:40:13 +0200

    hkjk

---
+++ program/practicas/practicas2/coches/RA2A1P1.py
@@ -0,0 +1,8 @@
+class coches: 
+    def __init__(self, marca, modelo, años_fabricacion):
+        self.marca = marca
+        self.modelo = modelo
+        self.año_fabricacion = años_fabricacion
+        self.velocidad = 0 #velocidad inicial 0 
+    
+        
